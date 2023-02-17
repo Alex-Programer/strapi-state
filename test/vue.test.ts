31880769342a28a56310d00538bd771f01ae5914
Alex-Programer <alex.lichangnan@gmail.com>
2023-02-17 16:50:48 +0800

    first commit

---
+++ test/vue.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest'
+import { useVue } from '..'
+
+
+describe('for vue', () => {
+  it('should work', async () => {
+    expect(useVue).toBeTruthy()
+  })
+})
