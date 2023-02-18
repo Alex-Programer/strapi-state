8a67e60cf6899a077ac1138fd28722582515e378
Alex-Programer <alex.lichangnan@gmail.com>
2023-02-18 10:57:12 +0800

    🦄 refactor:define type

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,24 @@
+type Type = "collection" | "single"
+
 interface Options {
   baseURL: string
   contentType: string
-  type?: "collection" | "single"
+  type?: Type
   apiToken?: string
+  auto?: boolean
+  debounce?: number
+}
+
+interface ProvideAPI {
+  load: () => void
+  getItem: () => void
+  createItem: () => void
+  deleteItem: () => void
+  updateItem: () => void
+}
+
+interface State<T, K extends Type> {
+  data: K extends "collection" ? T[] : T
+  error: Error | null
+  loading: boolean
 }
--- src/vue.ts
+++ src/vue.ts
@@ -1,7 +1,13 @@
-import { onMounted, onUnmounted, ref } from "vue"
+import { onMounted, onUnmounted, reactive, ref } from "vue"
 import { Strapi } from "./strapi.helper"
 
 export const useVue = (options: Options) => {
+  const state = reactive<State<{ title: string }, Type>>({
+    error: null,
+    loading: false,
+    data: [],
+  })
+
   const strapi = ref<Strapi>()
 
   onMounted(() => {
